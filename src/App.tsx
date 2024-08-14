42f104d7da211d38bbe0b8d04af715366c28e0b4
Katuniakz <katuniakz@gmail.com>
2024-08-14 21:33:10 +0700

    week 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./component/Login";
 import Register from "./component/Register";
+import Event from "./Event";
 
 function App() {
   return (
     <div className="App">
+      <Event />
       {/* <div className="container">
         <h1 className="title">Hello, React!</h1>
         <button className="button">Click Me</button>
---
+++ src/Event.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Event() {
+  return (
+    <div>
+        <h1>Event</h1>
+        <button onClick={() => alert('test click')}
+
+        >Click</button>
+    </div>
+  )
+}
+
+export default Event
